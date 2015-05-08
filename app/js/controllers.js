07e03a849461f2f3491d6d5fc34951eb539f4039
Alexander <awsaavedra@gmail.com>
2015-05-07 19:10:55 -0700

    added firebase functionality and directives to allow submitted information to be added to the online DB

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,13 @@ angular.module('myApp.controllers', [])
   .controller('LandingPageController', [function() {
 
   }])
-  .controller('WaitlistController',[function() {
+  .controller('WaitlistController',['$scope', '$firebase', function($scope, $firebase) {
+    var partiesRef = new Firebase('https://waitandeat-alexander.firebaseio.com/');
+    $scope.parties = $firebase(partiesRef);
 
+    $scope.party = { name: '', phone: '', size: ''};
+    $scope.saveParty = function(){
+      $scope.parties.$add($scope.party);
+      $scope.party = { name: '', phone: '', size: ''};
+    };
   }]);

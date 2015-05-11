bd022f71f0cf32fd87e9c74477fd4261e52b0097
Alexander <awsaavedra@gmail.com>
2015-05-11 14:43:38 -0700

    done? hooked up to firebase DB

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,13 +7,27 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('WaitlistController',['$scope', '$firebase', function($scope, $firebase) {
-    var partiesRef = new Firebase('https://waitandeat-alexander.firebaseio.com/');
+    //connecting scope.parties to live firebase data
+    var partiesRef = new Firebase('https://waitandeat-alexander.firebaseio.com/parties');
     $scope.parties = $firebase(partiesRef);
-
+    //object to store data to waitlist form
     $scope.newParty = { name: '', phone: '', size: ''};
 
+    //for saving parties to DB
     $scope.saveParty = function(){
       $scope.parties.$add($scope.newParty);
-      $scope.newParty = { name: '', phone: '', size: ''};
+      $scope.newParty = { name: '', phone: '', size: '', done:false };
+    };
+    //function to send text message to party
+    $scope.sendTextMessage = function(party){
+      var textMessageRef = new Firebase('https://waitandeat-alexander.firebaseio.com/textMessages');
+      var textMessages = $firebase(textMessageRef);
+      var newTextMessage = {
+        phoneNumber: party.phone,
+        size: party.size,
+        name: party.name,
+        done:false 
+      };
+      textMessages.$add(newTextMessage);
     };
   }]);

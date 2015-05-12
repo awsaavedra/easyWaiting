1f041c38d9d5ece20cd23844c4df903d2f6f695a
Alexander <awsaavedra@gmail.com>
2015-05-11 21:14:27 -0700

    url mistake

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,7 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', [])
-  .value('FIREBASE_URL', 'https://waitandeat-demo.firebaseio.com/')
+  .value('FIREBASE_URL', 'https://waitandeat-alexander.firebaseio.com/')
   .factory('authService', function($firebaseSimpleLogin, $location, FIREBASE_URL) {
     var authRef = new Firebase(FIREBASE_URL);
     var auth = $firebaseSimpleLogin(authRef);
